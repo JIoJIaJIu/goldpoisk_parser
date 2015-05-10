dc11b74ea6a75eca0eeeff88396e139237971861
Guro Bokum <jiojiajiu@gmail.com>
2015-05-10 14:24:45 +0700

    Refactoring

--- src/main/java/com.goldpoisk.app/Ftp.java
+++ src/main/java/com.goldpoisk.app/Ftp.java
@@ -10,33 +10,26 @@ import java.util.Arrays;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.io.CopyStreamAdapter;
+
 public class Ftp {
+	private String username = "";
+	private String password = "";
+	private String host = "";
+	private int port = 21;
+	private FTPClient client = null;
 	
-	private String username="";
-	private String password="";
-	private String host="";
-	private int port=21;
-	
-	private FTPClient client=null;
-	
-	public Ftp(String address, String username, String password){
-		
-		URL url = null;
-		
-		try {
-            url = new URL(address);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-		
-		this.host = url != null ? url.getHost() : null;
-		this.username=username;
-		this.password=password;
-		
+	public Ftp(String address, String username, String password)
+        throws MalformedURLException {
+
+		URL url = new URL(address);
+
+        this.host = url.getHost();
+        this.username = username;
+        this.password = password;
 	}
 	
-	boolean connect(){
-		boolean status=false;
+	boolean connect() {
+		boolean status = false;
 		
 		this.client = new FTPClient();
 		
@@ -45,9 +38,9 @@ public class Ftp {
 			client.enterLocalPassiveMode();
 			client.login(username, password);
 			client.setFileType(FTP.BINARY_FILE_TYPE);
-			status=true;
+			status = true;
 		} catch (Exception e) {
-			Parser.logger.error("Error while connect to ftp"+e.getMessage());
+			Parser.logger.error("Error while connect to ftp {}", e.getMessage());
 		}
 		
 		return status;
@@ -57,61 +50,56 @@ public class Ftp {
 		boolean status=false;
 		String dirname="";
 		
-		try{
+		try {
 			 String[] names = client.listNames();
 			 client.changeWorkingDirectory(names[0]);
-			 names=client.listNames();
+			 names = client.listNames();
 			 
-			 if(!Arrays.asList(names).contains(siteName)){
+			 if (!Arrays.asList(names).contains(siteName)) {
 				 client.makeDirectory(siteName);
 			 }
 			 
 			 client.changeWorkingDirectory(siteName);
 			 names = client.listNames();
 			 dirname="0.0.";
-			 dirname+=names.length;
+			 dirname += names.length;
 			 client.makeDirectory(dirname);
 			 client.changeWorkingDirectory(dirname);
-			 status=true;
-		}catch(Exception e){
-			Parser.logger.error("Ошибка! Не удалось создать каталог "+dirname+" в директории "+siteName);
+			 status = true;
+		} catch (Exception e) {
+			Parser.logger.error("Ошибка! Не удалось создать каталог {} в директории {}", dirname, siteName);
 		}
 		
 		return status;
 	}
 	
-	boolean saveFile(String filename,final String string){
-		
-		boolean status=false;
-		System.out.println("Запись дампа на ftp сервер...");
-         InputStream stream = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
-         try{
-        	
-        	 CopyStreamAdapter streamListener = new CopyStreamAdapter() {
+	boolean saveFile(String filename,final String string) {
+		Parser.logger.info("Запись дампа на ftp сервер...");
+		boolean status = false;
+        InputStream stream = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
 
-        		 int percent=0;
+        try {
+            CopyStreamAdapter streamListener = new CopyStreamAdapter() {
+                int percent = 0;
         		    
-        		    public void bytesTransferred(long totalBytesTransferred, int bytesTransferred, long streamSize) {
-        		       //this method will be called everytime some bytes are transferred
+                //this method will be called everytime some bytes are transferred
+                public void bytesTransferred(long totalBytesTransferred, int bytesTransferred, long streamSize) {
+                    int currentPercent = (int)(totalBytesTransferred * 100 / string.length());
+                    if (currentPercent >= percent + 10) {
+                        Parser.logger.debug("Процесс {}", percent);
+                        percent = currentPercent;
+                    }
+                }
+            };
 
-        		      int current_percent = (int)(totalBytesTransferred*100/string.length());
-        		      if(current_percent>=percent+10){
-        		    	  System.out.println("Процесс "+percent);
-        		    	  percent=current_percent;
-        		      }
-        		    }
-
-        		 };
-    		 client.setCopyStreamListener(streamListener);
-    		 client.storeFile(filename+".sql", stream);
-    		 Parser.logger.error("Файл успешно записан!");
-        	 status=true;
-         }catch(Exception e){
-        	 Parser.logger.error("Ошибка! Не удалось сохранить файл "+filename+".sql");
+            client.setCopyStreamListener(streamListener);
+            client.storeFile(filename + ".sql", stream);
+            Parser.logger.info("Файл успешно записан!");
+            status = true;
+         } catch(Exception e) {
+            Parser.logger.error("Ошибка! Не удалось сохранить файл {}.sql", filename);
          }
-         
-         
-         
+
          return status;
 	}
 }
